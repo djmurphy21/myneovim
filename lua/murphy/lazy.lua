1b15b0586ac36388a373b9e63ff7f99a7b5cb8f4
Dan Murphy <djmurphy21@gmail.com>
2023-11-03 22:36:17 -0500

    Fixed lazy lua

--- lua/murphy/lazy.lua
+++ lua/murphy/lazy.lua
@@ -11,4 +11,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "murphy.plugins" } })
+require("lazy").setup({ { import = "murphy.plugins" }, 
+	{ import = "murphy.plugins.lsp" } })
